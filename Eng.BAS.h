cbd718888163d0d3e10a081be7dde7a48f705318
Bernard2254 <bernard_menezes@hotmail.com>
2016-08-28 22:39:19 -0300

    Add files via upload
    Biblioteca geral;

---
+++ Eng.BAS.h
@@ -0,0 +1,11 @@
+char string[50]="Mascotinho e Cica games, apresentam...";
+char v[50];
+int global,b, c;
+double cor, cor2, cor3, alfa;
+GLuint texturaFundoMenu, texturaBotao, texturaBotaoPressionado, texturaJogar,
+texturaInstrucoes, texturaOpcoes, texturaSair;
+
+
+void atualiza(){
+  glutPostRedisplay();
+}
